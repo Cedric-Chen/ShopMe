8680d56dbf8f4ed4ddb066f8d3e75e6d5df582fb
HongtaiCao <hongtail2@illinois.edu>
2017-11-11 00:12:05 -0600

    datamodel test

---
+++ datamodel_test/business.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Business(object):
+    def get_business(self):
+        return {
+            'address': '1835 E Guadalupe Rd, Ste 106',
+            'city': 'Tempe',
+            'id': '--9QQLMTbFzLJ_oT-ON3Xw',
+            'is_open': 1,
+            'latitude': 33.3617,
+            'longitude': -111.91,
+            'name': 'Great Clips',
+            'neighborhood': '',
+            'postal_code': '85283',
+            'review_count': 9,
+            'stars': 3.0,
+            'state': 'AZ'
+        }
+
+model = Business()
---
+++ datamodel_test/photo.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Photo(object):
+    def get_photo(self):
+        return {
+            '-0JodhHzxFIIs1kp5RYN-Q': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': 'Nice but not really the sesame paste '
+                'cold noodle',
+                'id': '-0JodhHzxFIIs1kp5RYN-Q',
+                'label': 'food'
+            },
+            'Da9e0yl6xO-IyN-0GreJzQ': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': '',
+                'id': 'Da9e0yl6xO-IyN-0GreJzQ',
+                'label': 'food'
+            },
+            'FLPotnmmcTmDV5j3dwFH5g': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': 'shrimp lettuce wrap. delicious',
+                'id': 'FLPotnmmcTmDV5j3dwFH5g',
+                'label': 'food'
+            },
+            'FP2tY6_lA-PFHzmC7tYVkQ': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': 'Average',
+                'id': 'FP2tY6_lA-PFHzmC7tYVkQ',
+                'label': 'food'
+            },
+            'HzIKeS217nZhHqtnU17dtA': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': 'Interior',
+                'id': 'HzIKeS217nZhHqtnU17dtA',
+                'label': 'inside'
+            },
+            'goV5xJQHs4kLaJN5irdVDA': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': 'Most food we ordered are delicious! I '
+                'will go to here next time!',
+                'id': 'goV5xJQHs4kLaJN5irdVDA',
+                'label': 'food'
+            },
+            'ntBF1MuczNtxvMxFFD8u9Q': {
+                'business_id': '3cAy1gxlprn1ue0q3L_ziw',
+                'caption': '',
+                'id': 'ntBF1MuczNtxvMxFFD8u9Q',
+                'label': 'food'
+            }
+        }
+
+model = Photo()
