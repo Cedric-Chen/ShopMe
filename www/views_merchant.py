44495a22d2eeb9c680257db2760726aed0dae1c2
HongtaiCao <hongtail2@illinois.edu>
2017-11-13 16:49:11 -0600

    merge information, merchant, user pages. migrate backend apis.

--- www/views_merchant.py
+++ www/views_merchant.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import ast
+
 from flask import redirect, render_template, request
 #from flask_sqlalchemy import SQLAlchemy
 #from flask.ext.security import Security, SQLAlchemyUserDatastore,\
@@ -20,68 +22,62 @@ from datamodel.tip import tip
 from datamodel.user import user
 
 @app.route('/merchant/<business_id>/')
-def view_merchant():
-
-    merchant.business    = business.select(business_id)
-    merchant.attribute   = attribute.slect(business_id)
-    merchant.category    = category.select(business_id)
-    merchant.hours       = hours.select(business_id)
-    merchant.photo       = photo.select(business_id)
-
+def view_merchant(business_id):
     return render_template(
-        'merchant.html',
-        business = merchant.business,
-        attribute= merchant.attribute,
-        category = merchant.category,
-        hours    = merchant.hours,
-        photo    = merchant.photo
+        u'merchant.html',
+        attribute= attribute.select(business_id),
+        business = business.select(business_id),
+        category = category.select(business_id),
+        hours    = hours.select(business_id),
+        photo    = photo.select(business_id),
     )
 
 @app.route('/merchant/update_info/', methods=['POST'])
 def update_merchant_info():
-    business = {}
-
+    new_business = {}
     newname = request.form['newname']
-    if newname != '' and newname != merchant.business['name']:
-        business['name'] = newname
+    business_current = ast.literal_eval(request.args['business_key'])
+    if newname != '' and newname != business_current['name']:
+        new_business['name'] = newname
 
     newopenornot = request.form.get('newopenornot')
     if newopenornot:
-        if not merchant.business['is_open']:
-            business['is_open'] = 1
+        if not business_current['is_open']:
+            new_business['is_open'] = 1
     else:
-        if merchant.business['is_open']:
-            business['is_open'] = 0
+        if business_current['is_open']:
+            new_business['is_open'] = 0
 
     newstreet = request.form['newstreet']
-    if newstreet != '' and newstreet != merchant.business['address']:
-        business['address'] = newstreet
+    if newstreet != '' and newstreet != business_current['address']:
+        new_business['address'] = newstreet
     newcity = request.form['newcity']
-    if newcity != '' and newcity != merchant.business['city']:
-        business['city'] = newname
+    if newcity != '' and newcity != business_current['city']:
+        new_business['city'] = newname
     newstate = request.form['newstate']
-    if newstate != '' and newstate != merchant.business['state']:
-        business['state'] = newstate
+    if newstate != '' and newstate != business_current['state']:
+        new_business['state'] = newstate
     newneighborhood = request.form['newneighborhood']
-    if newneighborhood != '' and newneighborhood != merchant.business['neighborhood']:
-        business['neighborhood'] = newneighborhood
+    if newneighborhood != '' and newneighborhood != business_current['neighborhood']:
+        new_business['neighborhood'] = newneighborhood
     newpostal_code = request.form['newpostal_code']
-    if newpostal_code != '' and newpostal_code != merchant.business['postal_code']:
-        business['postal_code'] = newpostal_code
+    if newpostal_code != '' and newpostal_code != business_current['postal_code']:
+        new_business['postal_code'] = newpostal_code
     newlatitude = request.form['newlatitude']
-    if newlatitude != '' and newlatitude != merchant.business['latitude']:
-        business['latitude'] = newlatitude
+    if newlatitude != '' and newlatitude != business_current['latitude']:
+        new_business['latitude'] = newlatitude
     newlongitude = request.form['newlongitude']
-    if newlongitude != '' and newlongitude != merchant.business['longitude']:
-        business['longitude'] = newlongitude
-
+    if newlongitude != '' and newlongitude != business_current['longitude']:
+        new_business['longitude'] = newlongitude
+    business.update(business_current[u'id'], new_business, {})
     return redirect(request.referrer)
 
 @app.route('/merchant/update_attr/', methods=['POST'])
 def update_merchant_attr():
+    attribute_current = ast.literal_eval(request.args[u'attr_key'])
+    business_current = ast.literal_eval(request.args[u'business_key'])
     attr = {}
-
-    for field1, value1 in merchant.attribute.items():
+    for field1, value1 in attribute_current.items():
         if type(value1) == dict:
             for field2, value2 in value1.items():
                 newvalue = request.form.get(field1+'_'+field2)
@@ -101,17 +97,18 @@ def update_merchant_attr():
                 newvalue = False
             if newvalue != value1:
                 attr[field1] = newvalue
-
+    attribute.update(business_current[u'id'], attr, {})
     return redirect(request.referrer)
 
 @app.route('/merchant/update_hours/', methods=['POST'])
 def update_merchant_hours():
-    _hours = {}
+    business_current = ast.literal_eval(request.args[u'business_key'])
+    hours_current = ast.literal_eval(request.args[u'hours_key'])
     for date in ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
         'Friday', 'Saturday', 'Sunday']:
         newvalue = request.form[date+'_start_at'] + '-' + \
             request.form[date+'_end_at']
-        if newvalue != '-' and newvalue != merchant.hours[date]:
-            _hours[date] = newvalue
-
+        if newvalue != '-' and newvalue != hours_current[date]:
+            hours_current[date] = newvalue
+    hours.update(business_current[u'id'], hours_current, {})
     return redirect(request.referrer)
--- www/views_user.py
+++ www/views_user.py
@@ -20,20 +20,20 @@ from datamodel.user import user
 def view_user(user_id):
     friends = {}
     for one_id in friend.select(user_id):
-        friend = user.select(one_id)
-        if 'name' in friend:
-            friends[one_id] = friend
+        friend_dict = user.select(one_id)
+        if 'name' in friend_dict:
+            friends[one_id] = friend_dict
     return render_template('user.html',
-                           user=user.select(user_id),
-                           friend=friends
-                           )
+        user=user.select(user_id),
+        friend=friends
+    )
 
 @app.route('/friend/')
 def view_friend():
     url_for('static', filename='css/user.css')
     return render_template('friend.html',
-                           user=user.select("user_id not defined"),
-                           )
+        user=user.select("user_id not defined"),
+    )
 
 @app.route('/friend/remove/<friend_id>/<user_id>/')
 def remove_friend(friend_id, user_id):
@@ -46,4 +46,3 @@ def add_friend(friend_id, user_id):
 @app.route('/update/name/<name>/<user_id>/')
 def update_name(name, user_id):
     user.update(user_id,{"name":name})
-    pass
