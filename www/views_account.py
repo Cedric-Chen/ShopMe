f23611ee7aec561906ff95208de631a924b1158f
tanqiul <liutanqiu@gmail.com>
2017-11-25 18:53:24 -0600

    add account_user

--- www/views_account.py
+++ www/views_account.py
@@ -5,6 +5,7 @@ import ast
 
 from flask import redirect, render_template, request, session, \
     url_for, escape
+from datamodel.account_user import account_user
 
 #from flask_sqlalchemy import SQLAlchemy
 #from flask.ext.security import Security, \
@@ -24,22 +25,33 @@ def login():
 def check_user():
     username_form  = request.form['username']
     password_form  = request.form['password']
-    cur.execute("SELECT COUNT(1) FROM account_user WHERE username = %s;", 
-        [username_form]) # CHECKS IF USERNAME EXSIST
-    if cur.fetchone()[0]:
-        cur.execute("SELECT password FROM account_user \
-            WHERE username = %s;", 
-            [username_form]) # FETCH THE HASHED PASSWORD
-        for row in cur.fetchall():
-            if md5(password_form).hexdigest() == row[0]:
-                session['username'] = request.form['username']
-                return redirect(url_for('index'))
-            else:
-                error = "Invalid Credential"
+    
+    status, info = account_user.check(username_form, password_form)
+    
+    if status:
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
     else:
-        error = "Invalid Credential"
-    flask.flash(error)
-    return render_template('login.html', error=error)
+        flask.flash(info)
+        return redirect(url_for('login'))
+   
+#    from databse.datamodel import DataModel
+#    cur.execute("SELECT COUNT(1) FROM account_user WHERE username = %s;", \
+#        [username_form]) # CHECKS IF USERNAME EXSIST
+#    if cur.fetchone()[0]:
+#        cur.execute("SELECT password FROM account_user \
+#            WHERE username = %s;", 
+#            [username_form]) # FETCH THE HASHED PASSWORD
+#        for row in cur.fetchall():
+#            if md5(password_form).hexdigest() == row[0]:
+#                session['username'] = request.form['username']
+#                return redirect(url_for('index'))
+#            else:
+#                error = "Invalid Credential"
+#    else:
+#        error = "Invalid Credential"
+#    flask.flash(error)
+#    return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
