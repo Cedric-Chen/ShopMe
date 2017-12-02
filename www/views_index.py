d529cd283713f0af27ef41754c18e645bb45d848
tanqiul <liutanqiu@gmail.com>
2017-12-02 16:09:51 -0600

    get coords from ipinfo.io

--- www/views_index.py
+++ www/views_index.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import render_template
-
+from flask import render_template,request
+import requests, json
 from config import app
 
 # other views
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    user_ip = request.remote_addr
+    user_loc = json.loads(requests.get('https://ipinfo.io/%s/geo' % (user_ip)).content)['loc']
+    return render_template('index.html', userloc = user_loc)
 
 # only for modifying front-end page
 @app.route('/profile')
