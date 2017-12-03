9a44ae46762c0902baa1177e36c2cf2be5321468
cedirc chen <puyang.chen@gmail.com>
2017-12-03 16:56:38 -0600

    add admin_account

--- www/views_admin.py
+++ www/views_admin.py
@@ -7,70 +7,94 @@ import requests, json
 from config import app
 
 @login_required
-@app.route('/admin')
-def admin():
+@app.route('/admin_account')
+def admin_account():
     if session['account_type'] == 'admin':
         return render_template(
-            'admin.html'
+            'admin_account.html'
         )
     else:
         return redirect(request.args.get('next') or \
             request.referrer or \
-            url_for('index'))
+            url_for('/'))
 
 @login_required
-@app.route('/admin/search/<kw>', methods=['GET'])
-@app.route('/admin/search', methods=['POST'])
-def admin_search(kw=None):
+@app.route('/admin_database')
+def admin_database():
+    if session['account_type'] == 'admin':
+        return render_template(
+            'admin_database.html'
+        )
+    else:
+        return redirect(request.args.get('next') or \
+            request.referrer or \
+            url_for('/'))
+
+
+@login_required
+@app.route('/admin_account/search/<kw>', methods=['GET'])
+@app.route('/admin_account/search', methods=['POST'])
+def admin_account_search(kw=None):
     if session['account_type'] == 'admin':
         from datamodel.business import business
-        if not kw:
+ @login_required
+@app.route('/admin_account')
+def admin_account():
+    if session['account_type'] == 'admin':
+        return render_template(
+            'admin_account.html'
+        )
+    else:
+        return redirect(request.args.get('next') or \
+            request.referrer or \
+            url_for('/'))
+       if not kw:
             kw = request.form['kw']
         if kw == '':
-            return render_template('admin.html')
+            return render_template('admin_account.html')
         cond_kw = parse_keyword(kw)
         keys = list(cond_kw.keys())
 
         business_list = business.sort_by( \
             cond_kw, keys, [u'=']*len(keys), u'*', u'*')
         return render_template(
-            'admin.html',
+            'admin_account.html',
             business_list = business_list,
             kw = kw
         )
     else:
         return redirect(request.args.get('next') or \
             request.referrer or \
-            url_for('index'))
+            url_for('/'))
 
 @login_required
-@app.route('/admin/delete/')
-@app.route('/admin/delete/<kw>/<business_id>')
-def admin_delete(kw, business_id):
+@app.route('/admin_account/delete/')
+@app.route('/admin_account/delete/<kw>/<business_id>')
+def admin_account_delete(kw, business_id):
     if session['account_type'] == 'admin':
         from datamodel.business import business
         business.delete(business_id,business.select(business_id))
-        return redirect(url_for('admin_search') + '/' + kw)
+        return redirect(url_for('admin_account_search') + '/' + kw)
     else:
         return redirect(request.args.get('next') or \
             request.referrer or \
-            url_for('index'))
+            url_for('/'))
 
 @login_required
-@app.route('/admin/update/', methods=['POST'])
-@app.route('/admin/update/<kw>/<business_id>', methods=['POST'])
-def admin_update(kw, business_id):
+@app.route('/admin_account/update/', methods=['POST'])
+@app.route('/admin_account/update/<kw>/<business_id>', methods=['POST'])
+def admin_account_update(kw, business_id):
     if session['account_type'] == 'admin':
         from datamodel.account_business import account_business
         new_username = request.form['new_username']
         new_password = request.form['new_password']
         status,info = account_business.update(business_id, new_username, new_password)
         flash(info)
-        return redirect(url_for('admin_search') + '/' + kw)
+        return redirect(url_for('admin_account_search') + '/' + kw)
     else:
         return redirect(request.args.get('next') or \
             request.referrer or \
-            url_for('index'))
+            url_for('/'))
 
 def parse_keyword(kw):
     l = kw.replace(' ', '').split(',')
@@ -81,3 +105,5 @@ def parse_keyword(kw):
             v = x.split(':')[1]
             d[k] = v
     return d
+
+@
