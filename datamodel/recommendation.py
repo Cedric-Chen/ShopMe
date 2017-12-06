2a79cd8fae992513ea3998f76863bd14b9d0e6e1
cedirc chen <puyang.chen@gmail.com>
2017-12-06 00:23:40 -0600

    add dmrecommendation

---
+++ datamodel/recommendation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from database.dmrecommendation import DMRecommendation
+
+class Recommendation(DMRecommendation):
+    def __init__(self):
+        super().__init__(self)
+
+recommendation = Recommendation()
