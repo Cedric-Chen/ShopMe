e9e436c34d2cec2669e4d3957222cbb8c1583e2a
HongtaiCao <hongtail2@illinois.edu>
2017-12-05 22:33:32 -0600

    chat base system

--- hellow_flask.py
+++ hellow_flask.py
@@ -16,49 +16,54 @@
 @Description：配合html5测试服务端推送事件
 '''
 
+from config import app
 import random
+from datetime import datetime as dt
 import eventlet
 from flask_assets import Environment
 
 eventlet.monkey_patch()
+from database.mysql.engine import DbCtx
 from flask_cors import CORS, cross_origin
 from flask import Flask, Response, render_template
 from www.assets import bundles
 
-app = Flask(__name__)
 CORS(app)  # 注意这里的CORS
 assets_env = Environment(app)
 assets_env.register(bundles)
 
 
 @app.route('/wechat/')
-def hello_world():
+def hello_worlds():
     return render_template('chat.html')
 
 
 def event():
-
-    jj = 0
-    if True:
-        # l = len(message_box)
-            yield 'data:%s \n\n' % random.random()
-        # now = l
-        # eventlet.sleep(1)
-        # if random.random() > 0.3:
-        #     message_box.append(jj)
+    timestamp = dt.now()
+    db = DbCtx()
+    with db() as cursor:
+        ret = cursor.execute('SELECT * FROM wechat ORDER BY timestamp ASC')
+        result = cursor.fetchall()
+        if isinstance(result,list):
+            for item in result:
+                cursor.execute('DELETE FROM wechat WHERE timestamp=%s', (item[1],))
+                yield 'data:%s \n\n' % str(item[0])
+        db.commit()
 
 
 @app.route('/stream/', methods=['GET', 'POST'])
 def stream():
-    return Response(event(), mimetype="text/event-stream")  # 注意这里的响应类型
-
-def null():
-    yield ""
+    return Response(event(), mimetype="text/event-stream")  # response type
 
 
 @app.route('/message/<message>/')
 def add_message(message):
-    pass
+    db = DbCtx()
+    with db() as cursor:
+        args = (message, dt.now())
+        cursor.execute('INSERT INTO wechat (text, timestamp) VALUES (%s, %s)', args)
+        print(cursor.fetchall())
+        db.commit()
     # message_box.append(message)
     # print(message_box)
 
