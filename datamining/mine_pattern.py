b3df6a984a36d29a31a76289156078592b611231
cedirc chen <puyang.chen@gmail.com>
2017-12-20 10:56:25 -0600

    optimize mine pattern algorithm

--- datamining/mine_pattern.py
+++ datamining/mine_pattern.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 def mine_pattern(filename, min_sup):
     data = open(filename,'r')
@@ -22,34 +23,68 @@ def mine_pattern(filename, min_sup):
 
     tag = 2
     get_out = True
+    old_dict = dict_word
+    new_dict = {}
     while(True):
-        new_dict = dict(dict_word)
-        for word1, value1 in dict_word.items():
-            if len(word1) != tag - 1:
-                continue
-
-            for word2, value2 in dict_word.items():
-                if len(word2) != 1:
-                   continue
+        candidate = []
+        for word1, value1 in old_dict.items():
+            for word2, value2 in old_dict.items():
                 if word1 == word2:
                     continue
-                if word2.issubset(word1):
-                    continue
                 new_word = word1 | word2
-                if new_word in dict_word:
-                    continue
+                if len(new_word) == tag:
+                    candidate.append(new_word)
 
-                new_set = value1 & value2
-                if len(new_set) >= min_sup:
-                    new_dict[new_word] = new_set
-                    get_out = False
+        candidate = Counter(candidate)
+        for new_word, count in candidate.items():
+            if count != tag * (tag-1):
+                continue
+
+            value1 = old_dict[frozenset(list(new_word)[1:])]
+            value2 = old_dict[frozenset(list(new_word)[:-1])]
+            new_set = value1 & value2
+            if len(new_set) >= min_sup:
+                new_dict[new_word] = new_set
+                get_out = False
 
         if get_out:
             break
         get_out = True
-        dict_word = dict(new_dict)
+        dict_word = {**dict_word, **new_dict}
+        old_dict = new_dict
+        new_dict = {}
         tag += 1
     return dict_word
+#    tag = 2
+#    get_out = True
+#    while(True):
+#        new_dict = dict(dict_word)
+#        for word1, value1 in dict_word.items():
+#            if len(word1) != tag - 1:
+#                continue
+#
+#            for word2, value2 in dict_word.items():
+#                if len(word2) != 1:
+#                   continue
+#                if word1 == word2:
+#                    continue
+#                if word2.issubset(word1):
+#                    continue
+#                new_word = word1 | word2
+#                if new_word in dict_word:
+#                    continue
+#
+#                new_set = value1 & value2
+#                if len(new_set) >= min_sup:
+#                    new_dict[new_word] = new_set
+#                    get_out = False
+#
+#        if get_out:
+#            break
+#        get_out = True
+#        dict_word = dict(new_dict)
+#        tag += 1
+#    return dict_word
 
 if __name__ == '__main__':
     patterns = mine_pattern(sys.argv[1], int(sys.argv[2]))
